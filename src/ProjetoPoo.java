f8459d4d6e1a3f752611bfafcacb4f7553fb06f9
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2022-04-21 22:25:35 -0300

    Initial commit

---
+++ src/ProjetoPoo.java
@@ -0,0 +1,11 @@
+public class ProjetoPoo {
+
+    public static void main(String[] args) {
+
+     double calculo =4.0;
+       double calculo1=450.9;
+        double calculo2 =7.8;
+
+        System.out.println( calculo+calculo2+calculo2);
+    }
+}
